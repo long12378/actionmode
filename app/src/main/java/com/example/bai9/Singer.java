831a44ed54577b959c69b8b1657b18b95b838520
long12378 <nduclong7@gmail.com>
2020-10-22 22:28:53 +0700

    actionmode xoa nhieu item

---
+++ app/src/main/java/com/example/bai9/Singer.java
@@ -0,0 +1,33 @@
+package com.example.bai9;
+
+public class Singer {
+    private String Ten;
+    private String MoTa;
+
+
+    public String getTen() {
+        return Ten;
+    }
+
+    public void setTen(String ten) {
+        Ten = ten;
+    }
+
+    public String getMoTa() {
+        return MoTa;
+    }
+
+    public void setMoTa(String moTa) {
+        MoTa = moTa;
+    }
+
+
+
+    public Singer(String ten, String moTa){
+        Ten = ten;
+        MoTa = moTa;
+
+
+
+    }
+}
